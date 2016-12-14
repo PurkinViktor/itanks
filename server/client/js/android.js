8694c908a50ea4a75c997cf5f1597c0d0c6b37aa
Viktor <viktor_purkin@mail.ru>
2016-12-15 01:34:34 +0300

    логи

--- server/client/js/android.js
+++ server/client/js/android.js
@@ -16,13 +16,22 @@ var android = {
 
         this.dx = window.innerWidth / w;
         this.dy = window.innerHeight / h;
+        console.log("this.dx");
+        console.log(dx);
+        console.log("this.dy");
+
+        console.log(dy);
 
 
     },
     reCalcPosition: function (pos) {
         //{pageX:40, pageY: 40}
-        pos.pageX *= this.dx;
-        pos.pageY *= this.dy;
+        console.log("pos.pageX1");
+        console.log(pos.pageX);
+        pos.pageX = pos.pageX * this.dx;
+        pos.pageY = pos.pageY * this.dy;
+        console.log("pos.pageX2");
+        console.log(pos.pageX);
         return pos;
     },
     OnTouch: function (pos) {
