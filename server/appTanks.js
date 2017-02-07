5e6ecae58f2bac928c549f16a509c427191ed48a
Viktor <viktor_purkin@mail.ru>
2017-02-07 03:04:22 +0300

    конфиги для БД

--- server/appTanks.js
+++ server/appTanks.js
@@ -1,12 +1,24 @@
 var PORT = process.env.PORT || 5000;
 var HOST = process.env.HOST || '0.0.0.0';
 
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/iTanks';
+var NODE_ENV = process.env.NODE_ENV || 'local';
+
+console.log("=======================================================================");
+console.log({PORT: PORT, HOST: HOST, NODE_ENV: NODE_ENV, MONGODB_URI: MONGODB_URI});
+console.log("=======================================================================");
+
 
 var express = require('express');
 var http = require('http');
 //var session = require("express-session");
 var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/iTanks');
+//mongoose.connect('mongodb://dbuser_heroku:dbuser_heroku_@ds115738.mlab.com:15738/heroku_06rwlgxn');
+mongoose.connect(MONGODB_URI);
+
+//mongodb://<dbuser>:<dbpassword>@ds115738.mlab.com:15738/heroku_06rwlgxn
+
 var api = require('./controllers/api.js');
 var UserController = require('./controllers/UserController.js');
 
@@ -41,11 +53,11 @@ app.get('/', function (req, res) {
 //app.post('/thread', api.post);
 //app.get('/user/:name.:format?', api.show);
 var vAPI = "v1.0";
-app.get('/'+vAPI+'/user', UserController.getAll);
-app.post('/'+vAPI+'/user/create', UserController.create);
-app.post('/'+vAPI+'/user/create', UserController.create);
-app.post('/'+vAPI+'/user/logIn', UserController.logIn);
-app.get('/'+vAPI+'/user/logOut', UserController.logOut);
+app.get('/' + vAPI + '/user', UserController.getAll);
+app.post('/' + vAPI + '/user/create', UserController.create);
+app.post('/' + vAPI + '/user/create', UserController.create);
+app.post('/' + vAPI + '/user/logIn', UserController.logIn);
+app.get('/' + vAPI + '/user/logOut', UserController.logOut);
 
 
 var options = {
@@ -57,7 +69,7 @@ var cookie = require('cookie');
 
 io.set('authorization', function (data, accept) {
     // check if there's a cookie header
-    console.log(data.query);
+    //console.log(data.query);
     var login = data.query.login;
     var password = data.query.password;
 
