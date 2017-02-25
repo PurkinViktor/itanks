48e21b12643c0640817c887c3435721258ba4ebd
Viktor <viktor_purkin@mail.ru>
2017-02-25 19:31:09 +0300

    передача координат отдельным запросом, для ускорения обновления, для уменьшение задерки пинга

--- server/gameModule/CRenderingSystem.js
+++ server/gameModule/CRenderingSystem.js
@@ -12,17 +12,30 @@ module.exports = function (game) {
         // var d = new Date();
         // console.log("renderItem  ", data.position, d, " ", d.getMilliseconds());
 
+        item.getListFielsForSend = function () {
+            return ["id", "position", "direction"];
+        };
+        this.sendRequest(transportGame.sendUpdateDataItem, item);
+        item.getListFielsForSend = this.getListFielsForSend;
         this.sendRequest(transportGame.sendUpdateDataItem, item);
         // transportGame.sendUpdateDataItem(this.game.nameGame, data);
         // transportGame.sendData(data);
     };
     this.getListFielsForSend = function () {
-        return ["id", "teamId", "hp", "timeIntervalMove","timeIntervalFire", "countBullet", "name", "ownerId", "width", "height", "position", "direction", "speed", "typeObject"];
+        return ["id", "teamId", "hp", "timeIntervalMove", "timeIntervalFire", "countBullet", "name", "ownerId", "width", "height", "position", "direction", "speed", "typeObject"];
     };
     this.sendRequest = function (f, item) {
+
         if (this.game.isStart) {
             var argv = [];
-            var data = helper.cutInObj(item, this.getListFielsForSend());
+            item.getListFielsForSend = item.getListFielsForSend || this.getListFielsForSend;
+            var listFielsForSend = item.getListFielsForSend();
+            // if (item.getListFielsForSend == null) {
+            //     listFielsForSend = this.getListFielsForSend();
+            // }else{
+            //
+            // }
+            var data = helper.cutInObj(item, listFielsForSend);
 
             argv.push(this.game.nameGame);
             argv.push(data);
